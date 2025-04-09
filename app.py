bff0a611dbb86b6ad608f6898e4777c64daf24d2
Emi <emidesouches@gmail.com>
2025-04-09 19:12:52 -0300

    sessions listing bug fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import locale
 from datetime import datetime
+from collections import defaultdict
 locale.setlocale(locale.LC_TIME, '')
 
 def format_date(date_str):
@@ -65,14 +66,16 @@ def index():
         cursor.execute(query)
         data = cursor.fetchall()
 
-    grouped_sessions = {}
+    grouped_sessions = defaultdict(list)
     total = 0
+
     for person_id, name, session_id, date, price, pending in data:
-        if name not in grouped_sessions:
-            grouped_sessions[(person_id,name)] = []
+        grouped_session_key = (person_id,name)
+            
         if date and price:
             total += 1
-            grouped_sessions[(person_id,name)].append((session_id, format_date(date), format_price(price), pending))
+            session_data = (session_id, format_date(date), format_price(price), pending)
+            grouped_sessions[grouped_session_key].append(session_data)
 
     return render_template('index.html', grouped_sessions=grouped_sessions, filters=FILTERS, allow_delete=ALLOW_DELETE, show=show, total=total)
 
@@ -80,12 +83,15 @@ def index():
 def add_person():
     if request.method == 'POST':
         name = request.form.get('name')
+
         if name:
             with sqlite3.connect("database.db") as conn:
                 cursor = conn.cursor()
                 cursor.execute("INSERT OR IGNORE INTO persons (name) VALUES (?)", (name,))
                 conn.commit()
+
         return redirect(url_for('add_session'))
+
     return render_template('form_person.html')
 
 @app.route('/add_session', methods=['GET', 'POST'])
@@ -103,6 +109,7 @@ def add_session():
                     (person_id, session_date, session_price)
                 )
                 conn.commit()
+
         return redirect(url_for('index'))
 
     with sqlite3.connect("database.db") as conn:
@@ -118,18 +125,25 @@ def remove_session(id):
         cursor = conn.cursor()
         cursor.execute("DELETE FROM sessions WHERE id = ?", (id,))
         conn.commit()
+
     return redirect(url_for('index'))
 
-@app.route('/remove_person/<int:id>/<name>', methods=['GET', 'POST'])
-def remove_person(id, name):
+@app.route('/remove_person/<int:person_id>', methods=['GET', 'POST'])
+def remove_person(person_id):
     if request.method == 'POST':
         with sqlite3.connect("database.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("DELETE FROM sessions WHERE person_id = ?", (id,))    
-            cursor.execute("DELETE FROM persons WHERE id = ?", (id,))
+            cursor.execute("DELETE FROM sessions WHERE person_id = ?", (person_id,))    
+            cursor.execute("DELETE FROM persons WHERE id = ?", (person_id,))
             conn.commit()
+
         return redirect(url_for('index'))
-    return render_template('delete_person.html', id=id, name=name)
+
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        name = cursor.execute("SELECT name FROM persons WHERE id = ?", (person_id,)).fetchone()[0]  
+
+    return render_template('delete_person.html', person_id=person_id, name=name)
 
 @app.route('/toggle_pending/<int:id>')
 def toggle_pending(id):
@@ -138,22 +152,32 @@ def toggle_pending(id):
         pending = cursor.execute("SELECT pending FROM sessions WHERE id = ?", (id,)).fetchone()[0]
         cursor.execute("UPDATE sessions SET pending = ? WHERE id = ?", (not pending,id))
         conn.commit()
+
     return redirect(url_for('index'))
 
-@app.route('/update_session/<int:id>/<name>', methods=['GET', 'POST'])
-def update_session(id, name):
+@app.route('/update_session/<int:person_id>/<int:id>', methods=['GET', 'POST'])
+def update_session(person_id, id):
     if request.method == 'POST':
         session_date = request.form.get('session_date')
         session_price = request.form.get('session_price')
+
         with sqlite3.connect("database.db") as conn:
             cursor = conn.cursor()
-            cursor.execute("UPDATE sessions SET session_date = ?, session_price = ? WHERE id = ?", (session_date, session_price, id))
+            cursor.execute(
+                "UPDATE sessions SET session_date = ?, session_price = ? WHERE id = ?",
+                (session_date, session_price, id))
             conn.commit()
+
         return redirect(url_for('index'))
+
     with sqlite3.connect("database.db") as conn:
         cursor = conn.cursor()
-        date, price = cursor.execute("SELECT session_date,session_price FROM sessions WHERE id = ?", (id,)).fetchone()
-    return render_template('form_edit.html', id=id, date=date, price=price, name=name)
+        name, date, price = cursor.execute(
+            "SELECT persons.name,sessions.session_date,sessions.session_price \
+                FROM persons INNER JOIN sessions ON sessions.person_id=persons.id WHERE persons.id = ?",
+            (person_id,)).fetchone()
+    
+    return render_template('form_edit.html', id=id, person_id=person_id, date=date, price=price, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True)
